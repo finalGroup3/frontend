14fd7104ee737e56dd16cf618d736817ca94b3d5
AlaEmadIbrahim <ALA.EMAD.IBRAHIM@GMAIL.COM>
2023-09-30 00:34:30 +0300

    edit resturants

--- src/components/restaurants/Restaurants.jsx
+++ src/components/restaurants/Restaurants.jsx
@@ -1,27 +1,33 @@
 import "./restaurants.css" ;
-import service1 from "../../assets/service-1.jpg"
-import service2 from "../../assets/service-2.jpg"
-import service3 from "../../assets/service-3.jpg"
-import shape1 from "../../assets/shape-1.png"
-import shape2 from "../../assets/shape-2.png"
 
+import service1 from "../../assets/service-1.jpg";
+import service2 from "../../assets/service-2.jpg";
+import service3 from "../../assets/service-3.jpg";
+import shape1 from "../../assets/shape-1.png";
+import shape2 from "../../assets/shape-2.png";
+import event2 from "../../assets/event-2.jpg";
+import jubran from "../../assets/jubran.jpg";
+import burger from "../../assets/burger.jpg";
+import rest2 from "../../assets/rest2.png";
+import rest4 from "../../assets/rest4.jpg";
+import mansaf2 from "../../assets/mansaf2.jpg";
 
 
 export default function Restaurants() {
+ 
   return (
     <>
       <section
         className="section service bg-black-10 text-center"
         aria-label="service"
       >
-        <div className="container">
-          <p className="section-subtitle label-2">Flavors For Royalty</p>
-          <h2 className="headline-1 section-title">We Offer Top Notch</h2>
-          <p className="section-text">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry lorem Ipsum has been the industrys standard dummy text
-            ever.
-          </p>
+        <div className="container-top-three-resturants">
+          <br />
+          <p className="section-subtitle2 label-2">Flavors For Royalty</p>
+          <h2 className="headline-1 section-title">Top Resturants</h2>
+          <br />
+          <br />
+
           <ul className="grid-list">
             <li>
               <div className="service-card">
@@ -45,7 +51,7 @@ export default function Restaurants() {
                     <a href="#">Breakfast</a>
                   </h3>
                   <a href="#" className="btn-text hover-underline label-2">
-                    View Menu
+                    Book now
                   </a>
                 </div>
               </div>
@@ -72,7 +78,7 @@ export default function Restaurants() {
                     <a href="#">Appetizers</a>
                   </h3>
                   <a href="#" className="btn-text hover-underline label-2">
-                    View Menu
+                  Book now
                   </a>
                 </div>
               </div>
@@ -99,7 +105,7 @@ export default function Restaurants() {
                     <a href="#">Drinks</a>
                   </h3>
                   <a href="#" className="btn-text hover-underline label-2">
-                    View Menu
+                  Book now
                   </a>
                 </div>
               </div>
@@ -121,8 +127,151 @@ export default function Restaurants() {
             alt="shape"
             className="shape shape-2 move-anim"
           />
+
         </div>
+        
       </section>
+     
+   {/* --------------------------------- cards resturants------------------------------------- */}
+
+      <section className="section event bg-black-10" aria-label="event">
+  <div className="container">
+    <p className="section-subtitle label-2 text-center" id="Our-Resturants">Our Resturants</p>
+    <br />
+    <ul className="grid-list">
+      <li>
+        <div className="event-card has-before :shine">
+          <div className="card-banner img-holder" style={{"--width":"350","--height":"450"}}>
+            <img src={mansaf2} width={350} height={450} loading="lazy" alt="Flavour so good you’ll try to eat with your eyes." className="img-cover" />
+            <time className="publish-date label-2" dateTime="2022-09-15">⭐⭐⭐⭐</time>
+          </div>
+          <div className="card-content">
+          <p className="card-subtitle label-2 text-center">Mansaf</p>
+            <h3 className="card-title2  text-center">
+              Flavour so good try to eat with your eyes.
+              Flavour so good try to eat with your eyes.
+            </h3>
+            <br />
+            <p className="card-subtitle label-2 text-center">price: 10$</p> 
+            <br />
+            <a href="#" className="btn-text hover-underline label-2">
+                    Book now
+                  </a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div className="event-card has-before :shine">
+          <div className="card-banner img-holder" style={{"--width":"350","--height":"450"}}>
+            <img src={burger} width={350} height={450} loading="lazy" alt="Flavour so good you’ll try to eat with your eyes." className="img-cover" />
+            <time className="publish-date label-2" dateTime="2022-09-03">⭐⭐⭐⭐</time>
+          </div>
+          <div className="card-content">
+            <p className="card-subtitle label-2 text-center">burger</p>
+            <h3 className="card-title2  text-center">
+              Flavour so good try to eat with your eyes.
+              Flavour so good try to eat with your eyes.
+            </h3>
+            <br />
+            <p className="card-subtitle  label-2 text-center">price: 10$</p> 
+            <br />
+            <a href="#" className="btn-text hover-underline label-2">
+                    Book now
+                  </a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div className="event-card has-before :shine">
+          <div className="card-banner img-holder" style={{"--width":"350","--height":"450"}}>
+            <img src={event2} width={350} height={450} loading="lazy" alt="Flavour so good you’ll try to eat with your eyes." className="img-cover" />
+            <time className="publish-date label-2" dateTime="2022-09-08">⭐⭐⭐⭐</time>
+          </div>
+          <div className="card-content">
+            <p className="card-subtitle label-2 text-center">Hashem</p>
+            <h3 className="card-title2  text-center">
+              Flavour so good try to eat with your eyes.
+              Flavour so good try to eat with your eyes.
+            </h3>
+            <br />
+            <p className="card-subtitle label-2 text-center">price: 10$</p> 
+            <br />
+            <a href="#" className="btn-text hover-underline label-2">
+                    Book now
+                  </a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div className="event-card has-before :shine">
+          <div className="card-banner img-holder" style={{"--width":"350","--height":"450"}}>
+            <img src={jubran} width={350} height={450} loading="lazy" alt="Flavour so good you’ll try to eat with your eyes." className="img-cover" />
+            <time className="publish-date label-2" dateTime="2022-09-03">⭐⭐⭐⭐</time>
+          </div>
+          <div className="card-content">
+            <p className="card-subtitle label-2 text-center">Jubran</p>
+            <h3 className="card-title2  text-center">
+              Flavour so good try to eat with your eyes.
+              Flavour so good try to eat with your eyes.
+            </h3>
+            <br />
+            <p className="card-subtitle label-2 text-center">price: 10$</p> 
+            <br />
+            <a href="#" className="btn-text hover-underline label-2">
+                    Book now
+                  </a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div className="event-card has-before :shine">
+          <div className="card-banner img-holder" style={{"--width":"350","--height":"450"}}>
+            <img src={rest4} width={350} height={450} loading="lazy" alt="Flavour so good you’ll try to eat with your eyes." className="img-cover" />
+            <time className="publish-date label-2" dateTime="2022-09-03">⭐⭐⭐⭐</time>
+          </div>
+          <div className="card-content">
+            <p className="card-subtitle label-2 text-center">Falafel</p>
+            <h3 className="card-title2  text-center">
+              Flavour so good try to eat with your eyes.
+              Flavour so good try to eat with your eyes.
+            </h3>
+            <br />
+            <p className="card-subtitle label-2 text-center">price: 10$</p> 
+            <br />
+            <a href="#" className="btn-text hover-underline label-2">
+                    Book now
+                  </a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div className="event-card has-before :shine">
+          <div className="card-banner img-holder" style={{"--width":"350","--height":"450"}}>
+            <img src={rest2} width={350} height={450} loading="lazy" alt="Flavour so good you’ll try to eat with your eyes." className="img-cover" />
+            <time className="publish-date label-2" dateTime="2022-09-03">⭐⭐⭐⭐</time>
+          </div>
+          <div className="card-content">
+            <p className="card-subtitle label-2 text-center">Bake House</p>
+            <h3 className="card-title2 text-center">
+              Flavour so good try to eat with your eyes.
+              Flavour so good try to eat with your eyes.
+            </h3>
+            <br />
+            <p className="card-subtitle label-2 text-center">price: 10$</p> 
+            <br />
+            <a href="#" className="btn-text hover-underline label-2">
+                    Book now
+                  </a>
+          </div>
+        </div>
+      </li>
+    </ul>
+
+    
+  </div>
+  
+</section>
+
     </>
   );
 }
