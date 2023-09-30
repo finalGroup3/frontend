1bf8a2571edc6fa3bc1e3385c3ef7034bf21a730
LaithShatnawi <laith.sh66@gmail.com>
2023-09-30 18:25:01 +0000

    adding slider services and modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Hero from './components/home/heroSection/Hero'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Hero from "./components/home/heroSection/Hero";
+import Services from "./components/home/services/Services";
+import OurChoice from "./components/home/ourChoice/OurChouce";
+import OurChoice2 from "./components/home/ourChoice2/OurChouce2";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-<Hero/>
+      {/* <Services /> */}
+      {/* <OurChoice /> */}
+      <OurChoice2 />
+      {/* <Hero/> */}
       {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -31,7 +37,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
