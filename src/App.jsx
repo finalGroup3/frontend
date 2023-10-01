21d0d7baa867686b15337ec1209b4dd50ddc2364
LaithShatnawi <laith.sh66@gmail.com>
2023-10-01 17:36:59 +0000

    adding fav

--- src/App.jsx
+++ src/App.jsx
@@ -7,37 +7,21 @@ import Services from "./components/home/services/Services";
 import OurChoice from "./components/home/ourChoice/OurChouce";
 import OurChoice2 from "./components/home/ourChoice2/OurChouce2";
 import Restaurants from "./components/restaurants/Restaurants";
+import Footer from "./components/footer/Footer";
+import Favorites from "./components/favorites/Favorites";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Favorites />
       {/* <Services /> */}
       {/* <OurChoice /> */}
       {/* <OurChoice2 /> */}
       {/* <Hero/> */}
       {/* <Restaurants /> */}
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      {/* <Footer /> */}
     </>
   );
 }
---
+++ src/components/favorites/favCard/Card.jsx
@@ -0,0 +1,209 @@
+import "./Card.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUtensils } from "@fortawesome/free-solid-svg-icons";
+// import Heart from "react-animated-heart";
+// import { useState } from "react";
+
+const Card = () => {
+  // const [isClick, setClick] = useState(false);
+
+  // const handleSubmit = async (item) => {
+  //   try {
+  //     setClick((prevState) => ({
+  //       ...prevState,
+  //       [item.id]: !prevState[item.id],
+  //     }));
+  //     const serverUrl =` ${process.env.REACT_APP_SERVER_URL}addrestfav`;
+  //     const data = {
+  //       restname: item.name,
+  //       restimage: item.image,
+  //       restdescription: item.description,
+  //       restaddress: item.address,
+  //       restprice:item.price,
+  //       restrating: item.rating
+
+  //       };
+
+  //     console.log(data);
+  //     axios.post(serverUrl, data).then((res) => {
+  //         console.log(res);
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  return (
+    <div className="favsssss">
+      <ul className="cards">
+        <li className="listttt">
+          <div className="card">
+            <img
+              src="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg"
+              className="card__image"
+              alt=""
+            />
+            <div className="card__overlay">
+              <div className="card__header">
+                <svg className="card__arc" xmlns="http://www.w3.org/2000/svg">
+                  <path />
+                </svg>
+                <FontAwesomeIcon
+                  className="card__thumb"
+                  icon={faUtensils}
+                  style={{ color: "#001319" }}
+                />
+                <div className="card__header-text">
+                  <h3 className="card__title">Restaurant</h3>
+                  <span className="card__status">⭐⭐⭐⭐⭐</span>
+                </div>
+              </div>
+              <p className="card__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores, blanditiis?
+              </p>
+              <div className="containBtNn">
+                <button>Book Now!</button>
+                <button className="removeBTnnn">Remove</button>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li className="listttt">
+          <div className="card">
+            <img
+              src="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg"
+              className="card__image"
+              alt=""
+            />
+            <div className="card__overlay">
+              <div className="card__header">
+                <svg className="card__arc" xmlns="http://www.w3.org/2000/svg">
+                  <path />
+                </svg>
+                <FontAwesomeIcon
+                  className="card__thumb"
+                  icon={faUtensils}
+                  style={{ color: "#001319" }}
+                />
+                <div className="card__header-text">
+                  <h3 className="card__title">Restaurant</h3>
+                  <span className="card__status">⭐⭐⭐</span>
+                </div>
+              </div>
+              <p className="card__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores, blanditiis?
+              </p>
+              <div className="containBtNn">
+                <button>Book Now!</button>
+                <button className="removeBTnnn">Remove</button>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li className="listttt">
+          <div className="card">
+            <img
+              src="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg"
+              className="card__image"
+              alt=""
+            />
+            <div className="card__overlay">
+              <div className="card__header">
+                <svg className="card__arc" xmlns="http://www.w3.org/2000/svg">
+                  <path />
+                </svg>
+                <FontAwesomeIcon
+                  className="card__thumb"
+                  icon={faUtensils}
+                  style={{ color: "#001319" }}
+                />{" "}
+                <div className="card__header-text">
+                  <h3 className="card__title">Restaurant</h3>
+                  <span className="card__status">⭐⭐⭐⭐</span>
+                </div>
+              </div>
+              <p className="card__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores, blanditiis?
+              </p>
+              <div className="containBtNn">
+                <button>Book Now!</button>
+                <button className="removeBTnnn">Remove</button>
+              </div>
+            </div>
+          </div>
+        </li>
+        {/* <li className="listttt">
+          <div className="card">
+            <img
+              src="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg"
+              className="card__image"
+              alt=""
+            />
+            <div className="card__overlay">
+              <div className="card__header">
+                <svg className="card__arc" xmlns="http://www.w3.org/2000/svg">
+                  <path />
+                </svg>
+                <FontAwesomeIcon
+                  className="card__thumb"
+                  icon={faUtensils}
+                  style={{ color: "#001319" }}
+                />{" "}
+                <div className="card__header-text">
+                  <h3 className="card__title">Restaurant</h3>
+                  <span className="card__status">⭐⭐⭐⭐⭐</span>
+                </div>
+              </div>
+              <p className="card__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores, blanditiis?
+              </p>
+              <div className="containBtNn">
+                <button>Book Now!</button>
+                <button className="removeBTnnn">Remove</button>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li className="listttt">
+          <div className="card">
+            <img
+              src="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg"
+              className="card__image"
+              alt=""
+            />
+            <div className="card__overlay">
+              <div className="card__header">
+                <svg className="card__arc" xmlns="http://www.w3.org/2000/svg">
+                  <path />
+                </svg>
+                <FontAwesomeIcon
+                  className="card__thumb"
+                  icon={faUtensils}
+                  style={{ color: "#001319" }}
+                />{" "}
+                <div className="card__header-text">
+                  <h3 className="card__title">Restaurant</h3>
+                  <span className="card__status">⭐⭐⭐⭐⭐</span>
+                </div>
+              </div>
+              <p className="card__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores, blanditiis?
+              </p>
+              <div className="containBtNn">
+                <button>Book Now!</button>
+                <button className="removeBTnnn">Remove</button>
+              </div>
+            </div>
+          </div>
+        </li> */}
+      </ul>
+    </div>
+  );
+};
+
+export default Card;
