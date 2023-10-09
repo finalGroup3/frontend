4ecb4a0bbb335fa6d40e2d312813204bd6186f63
AmroBaniIssa <amro.nedal.issa@gmail.com>
2023-10-09 16:46:22 +0000

    .

--- src/components/reels/Modal/ReelModal.jsx
+++ src/components/reels/Modal/ReelModal.jsx
@@ -6,11 +6,12 @@ import axios from "axios";
 import cookie from "react-cookies";
 import { LoginContext } from "../../Auth/login/LogInContext";
 import { FaStar } from "react-icons/fa";
+import Dropdown from 'react-bootstrap/Dropdown';
 
 import "./ReelModal.scss";
 import Form from "react-bootstrap/Form";
 
-function ReelModal() {
+function ReelModal({ restId, hotelId, activId }) {
   const loginState = useContext(LoginContext);
 
   const [show, setShow] = useState(false);
@@ -18,7 +19,9 @@ function ReelModal() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [rating, setRating] = useState(null);
   const [hover, setHover] = useState(null);
-  console.log(rating)
+  const [category, setcategory] = useState("");
+
+  console.log(category);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -29,14 +32,27 @@ function ReelModal() {
   };
 
   const addToReels = () => {
-    const req = `${import.meta.env.VITE_DATABASE_URL}/reelsUpload`;
+    console.log(restId, hotelId, activId, "///////////");
 
+    const req = `${import.meta.env.VITE_DATABASE_URL}/reelsUpload`;
+    // const restaurantId = restId;
     const formData = new FormData();
     formData.append("username", loginState.user.username);
     formData.append("description", description);
     formData.append("rating", rating);
     formData.append("userId", loginState.user.id);
     formData.append("video", selectedFile);
+    formData.append("category", category);
+
+    if (restId) {
+      formData.append("restaurantId", restId);
+    }
+    if (hotelId) {
+      formData.append("hotelId", hotelId);
+    }
+    if (activId) {
+      formData.append("activityId", activId);
+    }
 
     axios
       .post(req, formData, {
@@ -112,16 +128,47 @@ function ReelModal() {
             <Form.Group controlId="formFile" className="mb-3">
               <Form.Label>Upload Video</Form.Label>
               <Form.Control type="file" onChange={handleFileChange} />
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Dropdown Button
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item
+                    href="#/action-1"
+                    value="rest"
+                    onClick={() => setcategory("restaurant")}
+                  >
+                    Restaurant
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    href="#/action-2"
+                    value="activity"
+                    onClick={() => setcategory("activity")}
+                  >
+                    Activity
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    href="#/action-3"
+                    value="hotel"
+                    onClick={() => setcategory("hotel")}
+                  >
+                    Hotel
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
             </Form.Group>
           </Form>
         </Modal.Body>
         <Modal.Footer>
+
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
           <Button className="bottanoo" variant="primary" onClick={addToReels}>
             Save Changes
           </Button>
+
         </Modal.Footer>
       </Modal>
     </div>
