bebac797b1f213d5ae8b0d66eb912ce473cb5e60
AlaEmadIbrahim <ALA.EMAD.IBRAHIM@GMAIL.COM>
2023-10-10 04:59:16 +0300

    design rest

--- src/components/restaurants/CCard.jsx
+++ src/components/restaurants/CCard.jsx
@@ -3,7 +3,8 @@ import { useState, useContext, useEffect } from "react";
 import { FavoritesContext } from "../favorites/favContext";
 // import Button from 'react-bootstrap/Button';
 import { ReelContext } from "../../components/reels/ReelsContext";
-
+import vieww from "../../assets/golddd.png";
+import favv from "../../assets/ggoolldd.png";
 import ReviewsModal from "../reels/reviewsmodal/Reviewsmodal";
 const CCard = ({ element, type }) => {
   const favstate = useContext(FavoritesContext);
@@ -48,27 +49,49 @@ const CCard = ({ element, type }) => {
             <img src={element.img} alt="Délicieux Bénédicte" />
           </div>
           <div className="item-text">
-            <p className="item-meal-type">{element.rating}⭐⭐⭐</p>
+            <p className="item-meal-type" >{element.rating}⭐⭐⭐</p>
+            <button onClick={favstate.AddToFavsDb(element)}>
+                    <img
+                      src={favv}
+                      alt="Review"
+                      className={`rotate-on-click ${modalShow ? "rotate" : ""}`}
+                      style={{
+                        width: "55px",
+                        height: "55px",
+                        marginLeft: "200px",
+                        marginTop: "-40px",
+                      }}
+                    />
+                  </button>
             <h2 className="item-title">{element.name}</h2>
             <p className="item-body">{element.description}</p>
-            <span>{element.location}</span>
+            {/* <span>{element.location}</span> */}
             <a
               className="btn-text hover-underline label-2"
               onClick={() => setOpenModal(true)}
             >
               Book now
             </a>
-            <button
-              className="btn-text hover-underline label-2"
-              onClick={() => favstate.AddToFavsDb(element)}
-            >
-              add to fav
-            </button>
+            <div className="product-links">
+                <div className="enlarged-photo-modal">
+                  <button onClick={() => setModalShow(true)}>
+                    <img
+                      src={vieww}
+                      alt="Review"
+                      style={{ width: "55px", height: "55px" ,marginLeft: "160px",
+                      marginTop: "-50px",}}
+                    />
+                    {/* <p style={{ width: "55px", height: "55px" ,marginLeft: "250px",
+                      marginTop: "-1px",}}>review</p> */}
+                  </button>
+                </div>
+                 
+                </div>
           </div>
-          <div className="item-price">{element.price}</div>
+          <div className="item-price">{element.price}JD</div>
         </div>
 
-        <button onClick={() => setModalShow(true)}>review</button>
+        
 
         <ReviewsModal
           show={modalShow}
