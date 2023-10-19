b221923bf0674a38d65ecfd2c837a7bc3109ebc9
AlaEmadIbrahim <ALA.EMAD.IBRAHIM@GMAIL.COM>
2023-10-19 16:20:51 +0300

    aaa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Set the base path to root for relative paths
+  build: {
+    outDir: 'build', // Specify the output directory for the build
+  },
 })
+
